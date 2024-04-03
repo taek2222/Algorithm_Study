5faefb96ab10038bdb3e09e3eb6317f257bb8a8c
marso34 <96871583+marso34@users.noreply.github.com>
2024-04-04 02:23:37 +0900

    04.09 대비 코딩 테스트 진행 중

---
+++ 04.08/오리_marso34.java
@@ -0,0 +1,57 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] input = br.readLine().toCharArray();
+        String quack = "quack";
+
+        int cnt = 0;
+
+        // while (true) {
+        //     int index = 0;
+
+        //     for (int i = 0; i < input.length; i++) {
+        //         if (input[i] == -1)
+        //             continue;
+
+        //         if (input[i] == quack.charAt(index)) {
+        //             input[i] = -1;
+        //             index++;
+        //         }
+
+        //         if (index == 5) {
+        //             cnt++;
+        //             break;
+        //         }
+        //     }
+
+        //     if (index < 5)
+        //         break;
+        // }
+
+        int[] arr = new int[5]; 
+
+        for (int i = 0; i < input.length; i++) {
+            arr[quack.indexOf(input[i])]++;
+
+            if (arr[0] > 0 && arr[1] > 0 && arr[2] > 0 && arr[3] > 0 && arr[4] > 0) {
+                cnt++;
+
+                arr[0]--;
+                arr[1]--;
+                arr[2]--;
+                arr[3]--;
+                arr[4]--;
+            }      
+        }
+
+        // 아직 테스트 중
+
+        cnt = cnt == 0 ? -1 : cnt;
+
+        System.out.println(cnt);
+    }   
+}
