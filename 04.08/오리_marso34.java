8f2772ae07383b27d456b54be0f4ce57cd20e5bb
marso34 <mars102aq@gmail.com>
2024-04-08 15:19:02 +0900

    Update 오리_marso34.java

--- 04.08/오리_marso34.java
+++ 04.08/오리_marso34.java
@@ -1,6 +1,7 @@
 ﻿import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -32,23 +33,55 @@ public class Main {
         //         break;
         // }
 
-        int[] arr = new int[5]; 
+        // int[] arr = new int[5]; 
+
+        // for (int i = 0; i < input.length; i++) {
+        //     arr[quack.indexOf(input[i])]++;
+
+        //     if (arr[0] > 0 && arr[1] > 0 && arr[2] > 0 && arr[3] > 0 && arr[4] > 0) {
+        //         cnt++;
+
+        //         arr[0]--;
+        //         arr[1]--;
+        //         arr[2]--;
+        //         arr[3]--;
+        //         arr[4]--;
+        //     }      
+        // }
+
+        ArrayList<String> list = new ArrayList<>();
 
         for (int i = 0; i < input.length; i++) {
-            arr[quack.indexOf(input[i])]++;
+            int index = quack.indexOf(input[i]);
 
-            if (arr[0] > 0 && arr[1] > 0 && arr[2] > 0 && arr[3] > 0 && arr[4] > 0) {
-                cnt++;
+            index = index == 0 ? 5 : index;
+            
+            if (i == 0) {
+                list.add(String.valueOf(input[i]));
+                continue;
+            }
 
-                arr[0]--;
-                arr[1]--;
-                arr[2]--;
-                arr[3]--;
-                arr[4]--;
-            }      
+            for (int j = 0; j < list.size(); j++) {
+                int length = list.get(j).length();
+
+                if (list.get(j).charAt(length-1) == quack.charAt(index-1)) {
+                    index = index == 5 ? 0 : index;
+
+                    list.set(j, list.get(j) + String.valueOf(quack.charAt(index)));
+                    break;
+                } else {
+                    list.add(String.valueOf(input[i]));
+                    break;
+                }
+            }
         }
 
-        // 아직 테스트 중
+        for (String str : list) {
+            if (str.contains(quack)) {
+                cnt++;
+            }
+            System.out.println(str);
+        }
 
         cnt = cnt == 0 ? -1 : cnt;
 
