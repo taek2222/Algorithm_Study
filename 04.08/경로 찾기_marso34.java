3aa818e56fdfd09a287325dd3d08896ad7d7d3d3
marso34 <96871583+marso34@users.noreply.github.com>
2024-04-08 16:07:45 +0900

    4문제 해결
    DNA, 베르트랑 공준, 오리, 경로 찾기

---
+++ 04.08/경로 찾기_marso34.java
@@ -0,0 +1,41 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        int[][] answer = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 거쳐가는 노드
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (arr[i][k] == 1 && arr[k][j] == 1) {
+                        arr[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
--- 04.08/오리_marso34.java
+++ 04.08/오리_marso34.java
@@ -9,7 +9,7 @@ public class Main {
         char[] input = br.readLine().toCharArray();
         String quack = "quack";
 
-        int cnt = 0;
+        // int cnt = 0;
 
         // while (true) {
         //     int index = 0;
@@ -49,42 +49,41 @@ public class Main {
         //     }      
         // }
 
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Character> list = new ArrayList<>();
 
+        loop1:
         for (int i = 0; i < input.length; i++) {
             int index = quack.indexOf(input[i]);
 
             index = index == 0 ? 5 : index;
             
-            if (i == 0) {
-                list.add(String.valueOf(input[i]));
-                continue;
-            }
-
             for (int j = 0; j < list.size(); j++) {
-                int length = list.get(j).length();
-
-                if (list.get(j).charAt(length-1) == quack.charAt(index-1)) {
+                if (list.get(j) == quack.charAt(index-1)) {
                     index = index == 5 ? 0 : index;
 
-                    list.set(j, list.get(j) + String.valueOf(quack.charAt(index)));
-                    break;
-                } else {
-                    list.add(String.valueOf(input[i]));
-                    break;
-                }
+                    list.set(j, quack.charAt(index));
+
+                    continue loop1;
+                } 
+            }
+
+            if (input[i] == 'q') {
+                list.add(input[i]);
+            } else {
+                System.out.println(-1);
+                return;
             }
         }
-
-        for (String str : list) {
-            if (str.contains(quack)) {
-                cnt++;
+        
+        int answer = list.size();
+        
+        for (char ch : list) {
+            if (ch != 'k') {
+                answer = -1;
+                break;
             }
-            System.out.println(str);
         }
-
-        cnt = cnt == 0 ? -1 : cnt;
-
-        System.out.println(cnt);
+        
+        System.out.println(answer);
     }   
 }
