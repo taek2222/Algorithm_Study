3c622a1050c5429aae617a3fe0144e9bb0313f90
marso34 <96871583+marso34@users.noreply.github.com>
2024-04-21 23:43:56 +0900

    04.23 baekjoon 1문제 추가 해결
    5번 문제 해결

---
+++ 04.23/4_2667_marso.java
@@ -0,0 +1,20 @@
+﻿import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        char[][] arr = new char[n][];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine().toCharArray();
+        }
+
+        // 알고리즘이 생각이 안 나..!!!!!!
+    }
+}
