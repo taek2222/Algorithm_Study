566e3b3b9e60ffebfb059743db112caaa3fd63da
marso34 <96871583+marso34@users.noreply.github.com>
2024-04-21 17:34:31 +0900

    04.16 baekjoon + 못 푼 문제 해결

---
+++ 04.23/1_1092_marso.java
@@ -0,0 +1,67 @@
+﻿import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Integer[] crane = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            crane[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        ArrayList<Integer> weight = new ArrayList<>();
+        
+        for (int i = 0; i < m; i++) {
+            weight.add(Integer.parseInt(st.nextToken()));
+        } // 여기까지 입력
+
+        // 내림차순 정렬
+        Arrays.sort(crane, Comparator.reverseOrder()); 
+        weight.sort(Collections.reverseOrder()); 
+
+        int cnt = 0;
+
+        if (crane[0] < weight.get(0)) { // 크레인으로 옮길 수 없으면
+            bw.write("-1");
+            bw.flush();   //남아있는 데이터를 모두 출력시킴
+            bw.close();   //스트림을 닫음
+            return;
+        }
+        
+        while(!weight.isEmpty()) {
+            int i = 0;
+
+            for (int c : crane) {
+                if (weight.isEmpty()) { // 옮기던 도중 끝나면
+                    break;
+                }
+                
+                for (; i < weight.size(); i++) {
+                    if (c >= weight.get(i)) { // 크레인으로 옮길 수 있으면
+                        weight.remove(i);
+                        break;
+                    }
+                }
+            }
+            
+            cnt++;
+        }
+    
+        bw.write(cnt + "");
+        bw.flush();   //남아있는 데이터를 모두 출력시킴
+        bw.close();   //스트림을 닫음
+    }
+}
---
+++ 04.23/2_2294_marso.java
@@ -0,0 +1,61 @@
+﻿import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        int[] arr = new int[k+1];
+
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        } // 여기까지 입력
+
+        Arrays.sort(coins);
+        Arrays.fill(arr, Integer.MAX_VALUE-1);
+        arr[0] = 0;
+
+        /*
+
+           1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+        1  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 
+        5  - - - - 1 1 1 1 1  2  2  2  2  2  3 
+        12 - - - - - - - - - -- --  1  1  1  1
+        
+           1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+        1  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 
+        5  1 2 3 4 1 2 3 4 5  2  3  4  5  6  3 <-- 기준 arr[6] = arr[6 -5(coin)] + 1 or arr[6] 중애 작은 거
+        12 1 2 3 4 1 2 3 4 5  2  3  1  2  3  3
+
+         */
+
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j <= k; j++) { // coins[i] 보다 작은 경우 무시
+                arr[j] = Math.min(arr[j - coins[i]] + 1, arr[j]); 
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        if (arr[k] == Integer.MAX_VALUE-1) {
+            bw.write("-1");
+            bw.flush();   //남아있는 데이터를 모두 출력시킴
+            bw.close();   //스트림을 닫음
+            return;
+        }
+
+        bw.write(arr[k]+"\n");
+        bw.flush();   //남아있는 데이터를 모두 출력시킴
+        bw.close();   //스트림을 닫음
+    }
+}
